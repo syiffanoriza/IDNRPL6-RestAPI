e585a422539922f147f8ac29594f996f8e0d52b1
syiffanoriza <syiffa.noriza@gmail.com>
2023-04-06 10:53:32 +0700

    v.06.4

--- app/Http/Middleware/isPostOwner.php
+++ app/Http/Middleware/isPostOwner.php
@@ -2,8 +2,10 @@
 
 namespace App\Http\Middleware;
 
+use App\Models\Post;
 use Closure;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 use Symfony\Component\HttpFoundation\Response;
 
 class isPostOwner
@@ -15,6 +17,13 @@ class isPostOwner
      */
     public function handle(Request $request, Closure $next): Response
     {
+        // 06 APR
+        $currentUser = Auth::User(); #cek user
+        $post = Post::findOrFail($request->id); #cek id post
+
+        if ($post->author != $currentUser->id) {
+            return response()->json(['message' => 'data not found'], 404); #return 404 klo bukan post owner
+        }
         return $next($request);
     }
 }
